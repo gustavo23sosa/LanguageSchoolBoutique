468534d530461728379d4211b3af6fd9662c5aed
Gustavo23sosa <tavovalencia491@gmail.com>
2023-07-21 23:06:40 -0600

    Cambios mejorados parte 2

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -42,17 +42,10 @@ class MasterController extends Controller
 	        //Recibimos el archivo y lo guardamos en la carpeta storage/app/public
 			$id = Auth::User()->id;
 		    // $user = $request['id_user'];
-		    $campos = [
 
-		    	'archivo' => 'required|max:15000|mimes:MP4,MOV,WMV,AVI,AVCHD,FLV,F4V,SWF,MKV,WEBM',
-		    ];
-		    $mensaje=[
-		    	'required'=>'El :attribute es requerido',
-		    	'archivo.required'=>'El archivo es requerido',
-		    ];
 
 		    $estatus = User::where('id', $id)->update([
-		    	'archivo' => $request->file('archivo')->store('uploads', 'public'), 
+		    	'archivo' => $request->file('archivo')->store('uploads', '/app/public'), 
 		    	'fk_nivel' => $request->input('nivel'), 
 		    	'fk_clases' => $request->input('clases')
 		    ]);
