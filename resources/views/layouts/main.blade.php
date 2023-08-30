7670b69676cc17e3e3d0fa380df9e0edbbf6983b
Gustavo23sosa <tavovalencia491@gmail.com>
2023-08-30 13:14:13 -0600

    cambios a produccion

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,8 +14,8 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href=" img/apple-icono.png">
-    <link rel="icon" type="image/png" href="img/icono.png">
+    <link rel="apple-touch-icon" sizes="76x76" href=" {{ asset('img/apple-icono.png') }}">
+    <link rel="icon" type="{{ asset('image/png') }}" href="{{ asset('img/icono.png') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     
     <title>
@@ -27,8 +27,8 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <!-- CSS Files -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
-    <link href="css/paper-dashboard.css?v=2.0.0" rel="stylesheet" />
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/paper-dashboard.css?v=2.0.0') }}" rel="stylesheet" />
 
 </head>
 
