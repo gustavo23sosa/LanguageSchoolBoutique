6cf3a20c6650a31a0af0be915b0e48a54f851c04
Gustavo23sosa <tavovalencia491@gmail.com>
2023-09-11 20:11:18 -0600

    Este cambio es para la pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <link rel="apple-touch-icon" sizes="76x76" href=" {{ asset('img/apple-icono.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('img/icono.png')}}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
