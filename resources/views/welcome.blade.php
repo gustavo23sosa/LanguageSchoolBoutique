6a4242f747451d37ddb3aa075fd442740acd3b92
Gustavo23sosa <tavovalencia491@gmail.com>
2023-09-11 20:53:30 -0600

    Este cambio es para la pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,21 @@
 
 <body>
 
-    <header class="hero" style="background-image: url({{('public/img/computer.jpg')}});">
+    <header class="hero">
+        <script type="css">
+            .hero::before {
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-size: cover;
+                background-image: linear-gradient(180deg, #0000008c 0%, #0000008c 100%), url("public/img/computer.jpg");
+                clip-path: polygon(0 0, 100% 0, 100% 80%, 50% 95%, 0 80%);
+                z-index: -1;
+            }
+        </script>
         <nav class="nav container">
             <div class="nav__logo">
                 <h2 class="nav__title">Curso CSS.</h2>
