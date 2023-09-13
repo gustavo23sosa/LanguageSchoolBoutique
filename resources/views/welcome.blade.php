c2a4f67ab54fcd148dfd33945e648d235c205a5e
Gustavo23sosa <tavovalencia491@gmail.com>
2023-09-12 22:35:16 -0600

    Este cambio es de diego para la pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
         <section class="hero__container container">
             
             <h1 class="hero__title">Juntos aprendemos un idioma y alcanzaremos tus metas.</h1>
-            <p class="hero__paragraph">Nuestra metodología, está especializada en clases hechas a la medida para las necesidades de cada alumno o grupo.</p>
+            <p class="hero__paragraph">Nuestra metodología, está especializada en clases hechas a la medida para las necesidades de cada alumno o grupo. Entra a realizar tu test para empezar con nosotros.</p>
             <a href="" class="cta">Comienza ahora</a>
         </section>
     </header>
@@ -118,46 +118,23 @@
             <h2 class="subtitle">Ellos ya conf&iacute;an en nosotros, ¡solo faltas t&uacute;!</h2>
 
             <div class="price__table">
+                
                 <div class="price__element">
-                    
-                    <h3 class="price__price">SEICONN</h3>
+                    <a href="https://www.seiconn.com.mx/"><img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%"></a>
 
-                    <div class="price__items">
-                        <img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%">
-                        <!-- <p class="price__features">Flexbox</p>
-                        <p class="price__features">Layouts</p>
-                        <p class="price__features">Responsive</p> -->
-                    </div>
+                    <a href="https://www.seiconn.com.mx/"><img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%"></a>
 
-                    <!-- <a href="#" class="price__cta">Empieza ahora</a> -->
+                    <a href="https://www.seiconn.com.mx/"><img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%"></a>
                 </div>
-
 
                 <div class="price__element price__element--best">
-                    <p class="price__name">Grid</p>
-                    <h3 class="price__price">$30/mes</h3>
 
-                    <div class="price__items">
-                        <p class="price__features">Grid</p>
-                        <p class="price__features">Implicit Grid</p>
-                        <p class="price__features">Responsive</p>
-                    </div>
+                    <a href="https://www.seiconn.com.mx/"><img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%"></a>
 
-                    <a href="#" class="price__cta">Empieza ahora</a>
-                </div>
+                    <a href="https://www.seiconn.com.mx/"><img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%"></a>
 
-
-                <div class="price__element">
-                    <p class="price__name">Animaciones</p>
-                    <h3 class="price__price">$40/mes</h3>
-
-                    <div class="price__items">
-                        <p class="price__features">Animation</p>
-                        <p class="price__features">Transition</p>
-                        <p class="price__features">Render Website</p>
-                    </div>
-
-                    <a href="#" class="price__cta">Empieza ahora</a>
+                    <a href="https://www.seiconn.com.mx/"><img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%"></a>
+                    
                 </div>
 
 
