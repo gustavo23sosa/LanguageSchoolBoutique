78d278cc4601d5eb0d58139e55288c8a48b93280
Gustavo23sosa <tavovalencia491@gmail.com>
2023-09-12 22:23:10 -0600

    Este cambio es de diego para la pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,7 @@
                     <h3 class="price__price">SEICONN</h3>
 
                     <div class="price__items">
-                        <img src="{{ asset('img/seiconn.jpg')}}" alt="">
+                        <img src="{{ asset('img/seiconn.jpg')}}" alt="" width="75%">
                         <!-- <p class="price__features">Flexbox</p>
                         <p class="price__features">Layouts</p>
                         <p class="price__features">Responsive</p> -->
